f193d93475b04ac98e7b17d9acc71fcf4566f2d0
Taczi <39707870+Taczi@users.noreply.github.com>
2018-12-18 21:29:45 +0100

    Update NetworkAnalyzerApplication.java

--- src/main/java/pl/put/poznan/analyzer/app/NetworkAnalyzerApplication.java
+++ src/main/java/pl/put/poznan/analyzer/app/NetworkAnalyzerApplication.java
@@ -3,10 +3,16 @@ package pl.put.poznan.analyzer.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * Main class that run the application
+ */
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.analyzer.rest"})
 public class NetworkAnalyzerApplication {
-
+    
+    /**
+     * Main function that starts the application
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(NetworkAnalyzerApplication.class, args);
     }
