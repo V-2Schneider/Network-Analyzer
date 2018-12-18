909388aa30c49d2c421efb06be904690bc00fb8f
Taczi <39707870+Taczi@users.noreply.github.com>
2018-12-18 21:09:24 +0100

    comment for javadoc

--- src/main/java/pl/put/poznan/analyzer/logic/Connection.java
+++ src/main/java/pl/put/poznan/analyzer/logic/Connection.java
@@ -1,15 +1,40 @@
 package pl.put.poznan.analyzer.logic;
 
+/**
+ * This class is used to manage a single connection
+ */
 class Connection {
+    /**
+     * Node from which the connection starts
+     */
     private Node from;
+    /**
+     * Node in which the connection ends
+     */
     private Node to;
+    /**
+     * Value of the connection
+     */
     private int value;
-
+    
+    /**
+     * Class constructor
+     *
+     * @param from  node from which the connection starts
+     * @param to    node in which the connection ends
+     * @param value value of the connection
+     */
     public Connection(Node from, Node to, int value) {
         this.from = from;
         this.to = to;
         this.value = value;
     }
+    
+    /**
+     * Override toString method
+     *
+     * @return string with all information about the object
+     */
 
     @Override
     public String toString() {
